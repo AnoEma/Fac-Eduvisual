a0b72cac7b8e73ca15ff847109fd2c8f4fef9cc4
Ano Emangi <ano.emangi@minutoseguros.com.br>
2021-04-01 11:50:16 -0300

    Criando controller de cadastro clientes

---
+++ Eduvisual.WebApi/Controllers/CadastroClienteController.cs
@@ -0,0 +1,48 @@
+﻿using Eduvisual.Application.InterfacesServices;
+using Eduvisual.Application.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net;
+
+namespace Eduvisual.WebApi.Controllers
+{
+    [ApiController]
+    [Route("cadastro-cliente")]
+    [Produces("application/json")]
+    public class CadastroClienteController : Controller
+    {
+        private readonly ICadastraClientesServices _services;
+
+        public CadastroClienteController(ICadastraClientesServices services)
+        {
+            _services = services;
+        }
+
+        [HttpGet]
+        [Route("clientes")]
+        public IActionResult Index()
+        {
+            var clientes = _services.GetAllClientes();
+            return Ok(clientes);
+        }
+
+        [HttpPost]
+        [Route("adicionar")]
+        public IActionResult AddClientes(CadastroClientesViewModel model)
+        {
+            try
+            {
+                _services.InsertCliente(model);
+                return StatusCode((int)HttpStatusCode.Created);
+            }
+            catch (ApplicationException ex)
+            {
+                return StatusCode((int)HttpStatusCode.BadRequest, ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+    }
+}
