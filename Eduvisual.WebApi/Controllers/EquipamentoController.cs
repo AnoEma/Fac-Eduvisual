e7fe56bd3c13972b98f8ba320cf4ca212c8c1d03
Ano Emangi <ano.emangi@minutoseguros.com.br>
2021-04-01 15:22:21 -0300

    Retirando as referencia

--- Eduvisual.WebApi/Controllers/EquipamentoController.cs
+++ Eduvisual.WebApi/Controllers/EquipamentoController.cs
@@ -2,10 +2,7 @@
 using Eduvisual.Application.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Threading.Tasks;
 
 namespace Eduvisual.WebApi.Controllers
 {
